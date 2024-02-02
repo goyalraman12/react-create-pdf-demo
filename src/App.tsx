bb315de8baa710f0d865b00b4e75f546d52a39c7
Raman Goyal <ramangoyal@MacBook-Air.local>
2024-02-02 19:00:14 +0530

    chore: Add a PDF Viewer inside a modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from "react-modal";
 import logo from "./logo.svg";
 import "./App.css";
 import MyPdf from "./MyPdf";
-import { PDFDownloadLink } from "@react-pdf/renderer";
+import { PDFDownloadLink, PDFViewer } from "@react-pdf/renderer";
 
 function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const urlSearchParams = new URLSearchParams(window.location.search);
   const nameFromUrl = urlSearchParams.get("name");
   const name = nameFromUrl || "James";
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
 
   /* Download button */
   const pdfDownloadLink = () => (
@@ -23,22 +33,48 @@ function App() {
   );
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h2>Hi {name}</h2>
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://www.npmjs.com/package/@react-pdf/renderer"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn more about ReactPdf
-        </a>
-        <p>Click the buttons below to preview or download your PDF</p>
+    <div className="App-container">
+      <h2>Hi {name}</h2>
+      <img src={logo} className="App-logo" alt="logo" />
+      <a
+        className="App-link"
+        href="https://www.npmjs.com/package/@react-pdf/renderer"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn more about ReactPdf
+      </a>
+      <p>Click the buttons below to preview or download your PDF</p>
 
-        <div className="buttons-container">{pdfDownloadLink()}</div>
-      </header>
+      <div className="buttons-container">
+        <button onClick={openModal}>Preview</button>
+
+        {pdfDownloadLink()}
+      </div>
+
+      {isModalOpen && (
+        <Modal
+          isOpen={isModalOpen}
+          onRequestClose={closeModal}
+          contentLabel="PDF Preview"
+          style={{
+            content: {
+              height: "80vh",
+              width: "70vw",
+              margin: "auto",
+              overflow: "hidden",
+            },
+          }}
+        >
+          <PDFViewer width="100%" height="550px">
+            <MyPdf name={name} />
+          </PDFViewer>
+          <button onClick={closeModal} style={{ marginRight: "16px" }}>
+            Close Preview
+          </button>
+          {pdfDownloadLink()}
+        </Modal>
+      )}
     </div>
   );
 }
